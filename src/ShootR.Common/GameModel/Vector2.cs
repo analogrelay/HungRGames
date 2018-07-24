2ed6967caec3df3d923531efa2c04fa7829f1eaa
Stephen Halter <halter73@gmail.com>
2018-07-24 15:18:17 -0700

    Add Vector2.Length and Distance

--- src/ShootR.Common/GameModel/Vector2.cs
+++ src/ShootR.Common/GameModel/Vector2.cs
@@ -1,4 +1,6 @@
-﻿namespace ShootR.GameModel
+﻿using System;
+
+namespace ShootR.GameModel
 {
     public class Vector2
     {
@@ -7,8 +9,13 @@
             X = x;
             Y = y;
         }
+
         public double X { get; }
         public double Y { get; }
+
+        public double Length => Math.Sqrt(X * X + Y * Y);
+
+        public double Distance(Vector2 vector) => (vector - this).Length;
 
         public static Vector2 operator *(Vector2 v1, Vector2 v2)
         {
@@ -69,7 +76,6 @@
         {
             return new Vector2(num - v1.X, num - v1.Y);
         }
-
 
         public override string ToString()
         {
