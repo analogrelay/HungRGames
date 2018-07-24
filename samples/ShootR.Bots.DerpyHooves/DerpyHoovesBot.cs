ee5da412ddba4b6a51f23b90e7e84722493f0613
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-07-24 14:25:25 -0700

    derpy lives!

--- samples/ShootR.Bots.DerpyHooves/DerpyHoovesBot.cs
+++ samples/ShootR.Bots.DerpyHooves/DerpyHoovesBot.cs
@@ -9,10 +9,11 @@ namespace ShootR.Bots.DerpyHooves
     public class DerpyHoovesBot
     {
         private readonly string _url;
-        private readonly BotClient.BotClient _client;
         private readonly GameTime _gameTime = new GameTime();
 
-        private readonly Timer _phaseTimer = new Timer(TimeSpan.FromSeconds(30));
+        internal BotClient.BotClient Client { get; }
+
+        private readonly Timer _phaseTimer = new Timer(TimeSpan.FromSeconds(10));
         private IPhase _activePhase;
 
         private readonly SpinningPhase _spinningPhase;
@@ -25,21 +26,27 @@ namespace ShootR.Bots.DerpyHooves
             _url = url;
 
             // Build the client
-            _client = new BotClient.BotClient(_url, new BotUserInformation("DerpyHooves", "https://i.kym-cdn.com/photos/images/newsfeed/000/122/114/130232443562.gif"));
-            _client.OnUpdateAsync += (context) =>
+            Client = new BotClient.BotClient(_url, new BotUserInformation("DerpyHooves", "https://i.kym-cdn.com/photos/images/newsfeed/000/122/114/130232443562.gif"));
+            Client.OnUpdateAsync += (context) =>
             {
                 _gameTime.Update(DateTime.UtcNow);
                 return TickAsync(context, CancellationToken);
             };
 
-            _spinningPhase = new SpinningPhase(_client);
-            _shootingPhase = new ShootingPhase(_client);
+            _spinningPhase = new SpinningPhase(this);
+            _shootingPhase = new ShootingPhase(this);
+        }
+
+        internal Task LogAsync(string message)
+        {
+            Console.WriteLine(message);
+            return Client.SendMessage(message, CancellationToken);
         }
 
         public async Task RunAsync(CancellationToken cancellationToken)
         {
             CancellationToken = cancellationToken;
-            await _client.ConnectAsync(cancellationToken);
+            await Client.ConnectAsync(cancellationToken);
 
             // Wait for shut down, the callback does all the work now.
             await cancellationToken.WaitForCancellationAsync();
@@ -47,7 +54,7 @@ namespace ShootR.Bots.DerpyHooves
 
         private async Task TickAsync(UpdateContext context, CancellationToken cancellationToken)
         {
-            if(_activePhase == null)
+            if (_activePhase == null)
             {
                 // Start with the spinning phase
                 _activePhase = _spinningPhase;
@@ -57,13 +64,13 @@ namespace ShootR.Bots.DerpyHooves
 
             _phaseTimer.Update(_gameTime);
 
-            if(_phaseTimer.HasElapsed)
+            if (_phaseTimer.HasElapsed)
             {
                 // Stop the active phase
                 await _activePhase.StopAsync();
 
                 // Swap phases
-                if(ReferenceEquals(_activePhase, _spinningPhase))
+                if (ReferenceEquals(_activePhase, _spinningPhase))
                 {
                     _activePhase = _shootingPhase;
                 }
@@ -74,6 +81,8 @@ namespace ShootR.Bots.DerpyHooves
 
                 // Start the next phase
                 await _activePhase.StartAsync();
+
+                _phaseTimer.Reset();
             }
 
             // Tick the active phase
@@ -89,32 +98,32 @@ namespace ShootR.Bots.DerpyHooves
 
         private class ShootingPhase : IPhase
         {
-            private BotClient.BotClient _client;
-            private Timer _shotTimer = new Timer(TimeSpan.FromMilliseconds(500));
+            private readonly DerpyHoovesBot _bot;
+            private Timer _shotTimer = new Timer(TimeSpan.FromMilliseconds(250));
 
-            public ShootingPhase(BotClient.BotClient client)
+            public ShootingPhase(DerpyHoovesBot bot)
             {
-                _client = client;
+                _bot = bot;
             }
 
             public async Task StartAsync()
             {
-                await _client.SendMessage("ACTIVATING SHOOTING UNIT.");
+                await _bot.LogAsync("ACTIVATING SHOOTING UNIT.");
             }
 
             public async Task StopAsync()
             {
-                await _client.SendMessage("DEACTIVATED SHOOTING MODULE.");
+                await _bot.LogAsync("DEACTIVATED SHOOTING MODULE.");
             }
 
             public async Task TickAsync(GameTime gameTime)
             {
                 _shotTimer.Update(gameTime);
 
-                if(_shotTimer.HasElapsed)
+                if (_shotTimer.HasElapsed)
                 {
-                    await _client.SendMessage("EMITTING FRIENDSHIP PARTICLE.");
-                    await _client.FireAsync();
+                    await _bot.LogAsync("EMITTING FRIENDSHIP PARTICLE.");
+                    await _bot.Client.FireAsync();
                     _shotTimer.Reset();
                 }
             }
@@ -122,23 +131,23 @@ namespace ShootR.Bots.DerpyHooves
 
         private class SpinningPhase : IPhase
         {
-            private readonly BotClient.BotClient _client;
+            private readonly DerpyHoovesBot _bot;
 
-            public SpinningPhase(BotClient.BotClient client)
+            public SpinningPhase(DerpyHoovesBot bot)
             {
-                _client = client;
+                _bot = bot;
             }
-            
+
             public async Task StartAsync()
             {
-                await _client.SendMessage("INITIATING SPINNING ROUTINES.");
-                await _client.StartMovementAsync(Movement.RotatingLeft);
+                await _bot.LogAsync("INITIATING SPINNING ROUTINES.");
+                await _bot.Client.StartMovementAsync(Movement.RotatingLeft);
             }
 
             public async Task StopAsync()
             {
-                await _client.SendMessage("DISENGAGING SPINNING PROTOCOL.");
-                await _client.StopMovementAsync(Movement.RotatingLeft);
+                await _bot.LogAsync("DISENGAGING SPINNING PROTOCOL.");
+                await _bot.Client.StopMovementAsync(Movement.RotatingLeft);
             }
 
             public Task TickAsync(GameTime gameTime)
